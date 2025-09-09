d67831a6cebfff3112993eb662ecb83389a6b86a
Chiwende <chiwendesakala@gmail.com>
2025-09-09 12:45:47 +0200

    removed redis connection

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-	"log"
-
 	"github.com/gin-gonic/gin"
-	"qinsights.com/thln/db"
 	"qinsights.com/thln/initializers"
 	"qinsights.com/thln/routes"
 )
@@ -13,11 +10,11 @@ func main() {
 	initializers.LoadEnv()
 
 	// Connect to Redis
-	redisClient, err := db.ConnectRedis(nil)
-	if err != nil {
-		log.Fatal("Failed to connect to Redis:", err)
-	}
-	defer db.CloseRedis(redisClient)
+	// redisClient, err := db.ConnectRedis(nil)
+	// if err != nil {
+	// 	log.Fatal("Failed to connect to Redis:", err)
+	// }
+	// defer db.CloseRedis(redisClient)
 
 	// db, err := db.Connect(nil)
 	// if err != nil {
